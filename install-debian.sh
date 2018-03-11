7ca346b8762f75eb1281b39625f28cae01f82deb
Ashley <ashley.davis@bbc.co.uk>
2018-03-11 00:00:54 +0000

    Remove unimportant debian package

--- install-debian.sh
+++ install-debian.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-PACKAGES="python3 python3-pip python3-venv libsdl2-dev python3-sdl2"
+PACKAGES="python3 python3-pip python3-venv libsdl2-dev"
 
 echo "Setting up ARDOR. This may take a short while."
 
-(which python3 &> /dev/null) || (echo -e "\033[0;31mERROR:\033[0m python3 not found.\nInstall it through your package manager: sudo apt install python3" && exit 1)
+(which python3 &> /dev/null) || (echo -e "\033[0;31mERROR:\033[0m python3 not found.\nInstall it through your package manager: sudo apt-get install $PACKAGES" && exit 1)
 
 (dpkg -l | grep libsdl2-dev &> /dev/null) || (echo -e "\033[0;31mERROR:\033[0m libsdl2-dev not found.\nInstall using your system package manager - e.g. sudo apt-get install $PACKAGES" && exit 1)
 
